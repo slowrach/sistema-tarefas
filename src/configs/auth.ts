009c66cc82bbdab0abf39c4eb52ca1e2af2b4029
Raquel Queiroz <113996452+slowrach@users.noreply.github.com>
2025-03-24 11:00:50 -0300

    correção de valor do expiresIn

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,8 +1,14 @@
 import { env } from "@/env"
+import { Secret } from "jsonwebtoken"
 
-export const authConfig = {
+export const authConfig: {
+   jwt: {
+      secret: Secret, 
+      expiresIn: `${number}D`
+   }
+} = {
    jwt: {
       secret: env.JWT_SECRET,
-      expiresIn: "1d",
+      expiresIn: "1D",
    }
 }
